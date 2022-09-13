b0a86f8b24c05317fabec705a2e8d4a4dab7f4da
Nex12315 <nikita20021804@gmail.com>
2022-09-13 14:00:35 +0300

    added files

---
+++ students_json.js
@@ -0,0 +1,382 @@
+// This is json format but it will be converted to js array / object at the end of the file
+// When you use students variable it will be a normal array because of the conversion at the end of the file
+var students = [
+  {
+    student_id: "123456",
+    firstname: "Horne",
+    lastname: "Hess",
+    gender: "male",
+    study_field: "Computer Science",
+    address: "745 Livingston Street, Cassel, South Dakota, 103",
+    email: "hornehess@magnafone.com",
+    phone: "+1 (877) 436-3132",
+    age: 20,
+    courses_done: [
+      {
+        course_id: "111",
+        course_name: "Programming 1",
+        credits: 8,
+      },
+      {
+        course_id: "112",
+        course_name: "Programming 2",
+        credits: 7,
+      },
+      {
+        course_id: "113",
+        course_name: "Web-Technologies",
+        credits: 4,
+      },
+      {
+        course_id: "114",
+        course_name: "Data Structures",
+        credits: 5,
+      },
+      {
+        course_id: "115",
+        course_name: "Mobile Programming",
+        credits: 5,
+      },
+      {
+        course_id: "116",
+        course_name: "Project 1",
+        credits: 5,
+      },
+      {
+        course_id: "117",
+        course_name: "Mathematics 1",
+        credits: 3,
+      },
+    ],
+  },
+  {
+    student_id: "111111",
+    firstname: "Bernadette",
+    lastname: "Roach",
+    gender: "female",
+    study_field: "Business",
+    address: "938 Eastern Parkway, Oley, Idaho, 8447",
+    email: "bernadetteroach@viagreat.com",
+    phone: "+1 (984) 489-3561",
+    age: 37,
+    courses_done: [
+      {
+        course_id: "200",
+        course_name: "Orientation",
+        credits: 7,
+      },
+      {
+        course_id: "201",
+        course_name: "Innovative Thinking",
+        credits: 7,
+      },
+      {
+        course_id: "202",
+        course_name: "International Business",
+        credits: 8,
+      },
+      {
+        course_id: "203",
+        course_name: "Accounting 1",
+        credits: 8,
+      },
+      {
+        course_id: "204",
+        course_name: "Accounting 2",
+        credits: 4,
+      },
+      {
+        course_id: "205",
+        course_name: "Marketing 1",
+        credits: 7,
+      },
+      {
+        course_id: "206",
+        course_name: "Marketing 2",
+        credits: 7,
+      },
+    ],
+  },
+  {
+    student_id: "111222",
+    firstname: "Knapp",
+    lastname: "Gamble",
+    gender: "male",
+    study_field: "Art",
+    address: "944 Poly Place, Jennings, Indiana, 1967",
+    email: "knappgamble@accruex.com",
+    phone: "+1 (912) 531-3662",
+    age: 22,
+    courses_done: [
+      {
+        course_id: "300",
+        course_name: "Basics of Art",
+        credits: 3,
+      },
+      {
+        course_id: "301",
+        course_name: "Orientation to studies",
+        credits: 10,
+      },
+      {
+        course_id: "302",
+        course_name: "Drawing Techniques 1",
+        credits: 6,
+      },
+      {
+        course_id: "303",
+        course_name: "Drawing Techniques 1",
+        credits: 4,
+      },
+      {
+        course_id: "304",
+        course_name: "Painting 1",
+        credits: 4,
+      },
+      {
+        course_id: "305",
+        course_name: "Painting 2",
+        credits: 5,
+      },
+      {
+        course_id: "306",
+        course_name: "Thesis",
+        credits: 7,
+      },
+    ],
+  },
+  {
+    student_id: "111333",
+    firstname: "Glenn",
+    lastname: "Arnold",
+    gender: "male",
+    study_field: "Art",
+    address: "163 Prospect Street, Whitestone, Wisconsin, 7045",
+    email: "glennarnold@bostonic.com",
+    phone: "+1 (996) 537-2027",
+    age: 26,
+    courses_done: [
+      {
+        course_id: "302",
+        course_name: "Drawing Techniques 1",
+        credits: 6,
+      },
+      {
+        course_id: "303",
+        course_name: "Drawing Techniques 1",
+        credits: 4,
+      },
+      {
+        course_id: "304",
+        course_name: "Painting 1",
+        credits: 4,
+      },
+      {
+        course_id: "305",
+        course_name: "Painting 2",
+        credits: 5,
+      },
+      {
+        course_id: "307",
+        course_name: "ART Studies 1",
+        credits: 8,
+      },
+      {
+        course_id: "308",
+        course_name: "Art Studies 2",
+        credits: 10,
+      },
+    ],
+  },
+  {
+    student_id: "111444",
+    firstname: "Caldwell",
+    lastname: "Wheeler",
+    gender: "male",
+    study_field: "Computer Science",
+    address: "269 Apollo Street, Waterford, American Samoa, 6614",
+    email: "caldwellwheeler@unq.com",
+    phone: "+1 (856) 516-3966",
+    age: 21,
+    courses_done: [
+      {
+        course_id: "111",
+        course_name: "Programming 1",
+        credits: 10,
+      },
+    ],
+  },
+  {
+    student_id: "111555",
+    firstname: "Marjorie",
+    lastname: "Oneal",
+    gender: "female",
+    study_field: "Computer Science",
+    address: "352 Central Avenue, Brutus, Michigan, 3144",
+    email: "marjorieoneal@softmicro.com",
+    phone: "+1 (853) 454-2876",
+    age: 29,
+    courses_done: [
+      {
+        course_id: "112",
+        course_name: "Programming 2",
+        credits: 7,
+      },
+      {
+        course_id: "113",
+        course_name: "Web-Technologies",
+        credits: 4,
+      },
+      {
+        course_id: "114",
+        course_name: "Data Structures",
+        credits: 5,
+      },
+      {
+        course_id: "115",
+        course_name: "Mobile Programming",
+        credits: 5,
+      },
+      {
+        course_id: "116",
+        course_name: "Project 1",
+        credits: 5,
+      },
+    ],
+  },
+  {
+    student_id: "111666",
+    firstname: "Duffy",
+    lastname: "Owens",
+    gender: "male",
+    study_field: "Business",
+    address: "814 Neptune Court, Keller, Tennessee, 5897",
+    email: "duffyowens@farmage.com",
+    phone: "+1 (831) 445-2693",
+    age: 26,
+    courses_done: [
+      {
+        course_id: "200",
+        course_name: "Orientation",
+        credits: 7,
+      },
+      {
+        course_id: "201",
+        course_name: "Innovative Thinking",
+        credits: 7,
+      },
+      {
+        course_id: "202",
+        course_name: "International Business",
+        credits: 8,
+      },
+      {
+        course_id: "203",
+        course_name: "Accounting 1",
+        credits: 8,
+      },
+      {
+        course_id: "204",
+        course_name: "Accounting 2",
+        credits: 4,
+      },
+      {
+        course_id: "205",
+        course_name: "Marketing 1",
+        credits: 7,
+      },
+    ],
+  },
+  {
+    student_id: "111777",
+    firstname: "Sharron",
+    lastname: "Hahn",
+    gender: "female",
+    study_field: "Business",
+    address: "624 Christopher Avenue, Nescatunga, Massachusetts, 3501",
+    email: "sharronhahn@cosmosis.com",
+    phone: "+1 (905) 511-3179",
+    age: 40,
+    courses_done: [
+      {
+        course_id: "200",
+        course_name: "Orientation",
+        credits: 7,
+      },
+      {
+        course_id: "203",
+        course_name: "Accounting 1",
+        credits: 8,
+      },
+      {
+        course_id: "204",
+        course_name: "Accounting 2",
+        credits: 4,
+      },
+      {
+        course_id: "205",
+        course_name: "Marketing 1",
+        credits: 7,
+      },
+    ],
+  },
+  {
+    student_id: "111888",
+    firstname: "Lucas",
+    lastname: "Sanchez",
+    gender: "male",
+    study_field: "Art",
+    address: "972 Rutland Road, Munjor, North Dakota, 4550",
+    email: "lucassanchez@isodrive.com",
+    phone: "+1 (992) 527-3722",
+    age: 25,
+    courses_done: [
+      {
+        course_id: "302",
+        course_name: "Drawing Techniques 1",
+        credits: 6,
+      },
+    ],
+  },
+  {
+    student_id: "111999",
+    firstname: "Sosa",
+    lastname: "Schmidt",
+    gender: "male",
+    study_field: "Art",
+    address: "993 Banner Avenue, Ronco, West Virginia, 5248",
+    email: "sosaschmidt@rockyard.com",
+    phone: "+1 (848) 420-2418",
+    age: 34,
+    courses_done: [
+      {
+        course_id: "301",
+        course_name: "Orientation to studies",
+        credits: 10,
+      },
+      {
+        course_id: "302",
+        course_name: "Drawing Techniques 1",
+        credits: 6,
+      },
+      {
+        course_id: "303",
+        course_name: "Drawing Techniques 1",
+        credits: 4,
+      },
+      {
+        course_id: "304",
+        course_name: "Painting 1",
+        credits: 4,
+      },
+      {
+        course_id: "305",
+        course_name: "Painting 2",
+        credits: 5,
+      },
+    ],
+  },
+];
+var students = JSON.stringify(students); // Makes a string from the array so that we can parse it then to js array
+students = JSON.parse(students); // students will be an array (object) now
+// Thanks for that
